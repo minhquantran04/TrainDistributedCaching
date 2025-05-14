79d9c8c89318b178be6537348b31615cabb3d8a3
minhquantran04 <quantmce181985@fpt.edu.vn>
2025-05-14 13:50:23 +0700

    Get All

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,53 @@
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Logging.Abstractions;
+using TrainDistributedCaching.Models;
+
+namespace TrainDistributedCaching.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        private readonly IDistributedCache _cache;
+
+        // logger để ghi log ra console hoặc file
+        private readonly ILogger<ProductsController> _logger;
+        public ProductsController(AppDbContext context, IDistributedCache cache, ILogger<ProductsController> logger)
+        {
+            _context = context;
+            _cache = cache;
+            _logger = logger;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProduct()
+        {
+            const string cacheKey = "products_all";
+            //lấy data từ cache
+            var cachedData = await _cache.GetStringAsync(cacheKey);
+            if (cachedData != null)
+            {
+                _logger.LogInformation(" Data is get from distributed cache(SQL)");
+                var cachedProduct = JsonSerializer.Deserialize<Product>(cachedData);
+                return Ok(cachedProduct);
+            }
+            // 2. Nếu không có cache → truy vấn DB
+            var products = await _context.Products.ToListAsync();
+            // 3. Lưu vào cache
+            var options = new DistributedCacheEntryOptions()
+                .SetSlidingExpiration(TimeSpan.FromMinutes(5)); // hết hạn nếu không dùng 5 phút
+
+            var jsonData = JsonSerializer.Serialize(products);
+            await _cache.SetStringAsync(cacheKey, jsonData, options);
+
+            _logger.LogInformation(" Lấy từ DB và lưu vào Distributed Cache");
+            return Ok(products);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using TrainDistributedCaching.Models;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TrainDistributedCaching
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddDistributedSqlServerCache(options =>
+            {
+                options.ConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+                options.SchemaName = "dbo";
+                options.TableName = "CacheTable";
+            });
+            // DbContext cho Products
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
